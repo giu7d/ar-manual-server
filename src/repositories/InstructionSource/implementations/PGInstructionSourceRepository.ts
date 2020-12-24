56efca3fab50497eb8fef861ea565c21e8d398c3
Giuseppe Setem <giuseppe7d@gmail.com>
2020-12-24 01:42:33 +0000

    fix: remove analysis-source instruction-id

--- src/repositories/InstructionSource/implementations/PGInstructionSourceRepository.ts
+++ src/repositories/InstructionSource/implementations/PGInstructionSourceRepository.ts
@@ -2,7 +2,6 @@ import { getRepository, Repository } from "typeorm";
 
 import { InstructionSource } from "src/entities/InstructionSource/InstructionSource";
 import { InstructionSourceORM } from "src/entities/InstructionSource/InstructionSourceORM";
-import { ApplicationError } from "src/utils";
 
 import { IInstructionSourceRepository } from "../IInstructionSourceRepository";
 
@@ -13,17 +12,6 @@ export class PGInstructionSourceRepository
 	}
 
 	async save(instructionId: string, instructionSource: InstructionSource) {
-		if (!instructionId) {
-			throw new ApplicationError(400, "Instruction ID needs to be passed!");
-		}
-
-		if (!instructionSource) {
-			throw new ApplicationError(400, "Instruction Source needs to be passed!");
-		}
-
-		await this.repository().save({
-			...instructionSource,
-			instructionId,
-		} as InstructionSourceORM);
+		await this.repository().save(instructionSource);
 	}
 }
